67ac53a5acd7308331171303465ad7c224241670
Antonio Stoilkov <hello@astoilkov.com>
2022-06-16 13:43:10 +0300

    🐛 fix ESLint error

--- src/yieldOrContinue.ts
+++ src/yieldOrContinue.ts
@@ -1,6 +1,8 @@
 import isTimeToYield from './isTimeToYield'
 import yieldControl from './yieldControl'
 
+// disabling ESLint otherwise `requestPromiseEscape()` in `yieldControl()` won't work
+// eslint-disable-next-line @typescript-eslint/promise-function-async
 export default function yieldOrContinue(priority: 'user-visible' | 'background'): Promise<void> {
     if (isTimeToYield(priority)) {
         return yieldControl(priority)
