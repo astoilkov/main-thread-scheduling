b32fe51f2f0992d8fcc878a6b15ca10a48093be9
Antonio Stoilkov <hello@astoilkov.com>
2023-08-23 11:32:32 +0300

    ✏️ add afterFrame() inspiration source

--- src/afterFrame.ts
+++ src/afterFrame.ts
@@ -1,5 +1,6 @@
 import queueTask from './queueTask'
 
+// inspired by: https://github.com/andrewiggins/afterframe
 export default function afterFrame(callback: () => void): void {
     requestAnimationFrame(() => {
         queueTask(callback)
