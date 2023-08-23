917c70f0d36261a423d4e099c6f5040c44559255
Antonio Stoilkov <hello@astoilkov.com>
2023-08-23 12:31:00 +0300

    ✏️ better doc

--- src/afterFrame.ts
+++ src/afterFrame.ts
@@ -1,6 +1,11 @@
 import queueTask from './queueTask'
 
-// inspired by: https://github.com/andrewiggins/afterframe
+/**
+ * Calls the callback after the browser renders the next frame.
+ * Compared to requestAnimationFrame() that calls the callback before the next frame.
+ * Inspired by: https://github.com/andrewiggins/afterframe
+ * @param callback
+ */
 export default function afterFrame(callback: () => void): void {
     requestAnimationFrame(() => {
         queueTask(callback)
