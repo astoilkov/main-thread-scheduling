814f26f183748c1d4402517eba927c12876b7ec3
Antonio Stoilkov <hello@astoilkov.com>
2024-01-31 11:22:47 +0200

    🐛 idle should wait after frame

--- src/WorkCycleTracker.ts
+++ src/WorkCycleTracker.ts
@@ -29,7 +29,7 @@ export default class WorkCycleTracker {
             if (ricTracker.available) {
                 await ricTracker.waitIdleCallback()
             } else {
-                await frameTracker.waitAnimationFrame()
+                await frameTracker.waitAfterFrame()
             }
         }
 
